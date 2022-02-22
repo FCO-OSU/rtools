2385bc1151acfe0d396e1c62ecd377c25ded4995
osu <>
2022-02-22 14:23:34 +0000

    mercator reprojections

--- R/geospatial.R
+++ R/geospatial.R
@@ -1,3 +1,41 @@
+#' @title Reproject points
+#' @description Convert Lat-Lons (WGS84) to Mercator
+#' @name latlon_to_merc
+#' @param lon A numeric value or vector of longitude (East-West) values
+#' @param lat A numeric value or vector of latitude (North-South) values
+#' @rdname latlon_to_merc
+#' @return A data.frame of Mercator coordinates
+#' @examples
+#' latlon_to_merc(lon = 1, lat = 52)
+#' @export
+
+latlon_to_merc = function(lon, lat)
+{
+  sf::sf_project(from = "+proj=longlat +datum=WGS84 +no_defs",
+                 to = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs",
+                 pts = cbind(lon, lat)) |> as.data.frame() |> `names<-`(c('x','y'))
+}
+
+
+#' @title Reproject points
+#' @description Convert Mercator to Lat-Lons (WGS84)
+#' @name merc_to_latlon
+#' @param lon A numeric value or vector of Mercator East-West values
+#' @param lat A numeric value or vector of Mercator North-South values
+#' @rdname merc_to_latlon
+#' @return A data.frame of WGS84 coordinates
+#' @examples
+#' merc_to_latlon(lon = 1, lat = 52)
+#' @export
+
+merc_to_latlon = function(x, y)
+{
+  sf::sf_project(from = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs",
+                 to = "+proj=longlat +datum=WGS84 +no_defs",
+                 pts = cbind(x, y)) |> as.data.frame() |> `names<-`(c('lon','lat'))
+}
+
+
 #' @title Web Map Tiles
 #' @description Convert Web Map Tile coordinates to Mercator
 #' @name wmt_to_merc
