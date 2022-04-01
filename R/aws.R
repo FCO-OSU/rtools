b26ed1d5df296451394d2d53fdcebe768b48b2f8
diplodata <robin.edwards@digital.fco.gov.uk>
2022-04-01 11:03:43 +0100

    aws param2

--- R/aws.R
+++ R/aws.R
@@ -1,15 +1,15 @@
 #' @title AWS tools
 #' @description Retrieve AWS secret
-#' @name aws_get_secret
+#' @name aws_get_param
 #' @param secret_id Character string AWS parameter name
 #' @param region Character string AWS region
-#' @rdname aws_get_secret
+#' @rdname aws_get_param
 #' @return A character string
 #' @examples
-#' aws_get_secret("secret-name")
+#' aws_get_param("param-name")
 #' @export
 
-aws_get_secret = function(secret_id, region = 'eu-west-2'){
+aws_get_param = function(secret_id, region = 'eu-west-2'){
   ssm <- paws::ssm(config = list(region = region))
   api_parameter <- ssm$get_parameter(secret_id, WithDecryption = TRUE)
   api_parameter$Parameter$Value
