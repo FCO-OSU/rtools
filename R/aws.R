d74e96785cda4d9fadb29872bf3a7b78731e4eb6
diplodata <robin.edwards@digital.fco.gov.uk>
2022-04-04 13:56:15 +0100

    aws_get_param v2

--- R/aws.R
+++ R/aws.R
@@ -1,16 +1,22 @@
 #' @title AWS tools
-#' @description Retrieve AWS secret
+#' @description Retrieve an AWS parameter via SSM
 #' @name aws_get_param
-#' @param secret_id Character string AWS parameter name
+#' @param parameter Character string AWS parameter name
 #' @param region Character string AWS region
+#' @param profile Character string name of AWS CLI profile if required
 #' @rdname aws_get_param
 #' @return A character string
 #' @examples
 #' aws_get_param("param-name")
 #' @export
 
-aws_get_param = function(secret_id, region = 'eu-west-2'){
-  ssm <- paws::ssm(config = list(region = region))
-  api_parameter <- ssm$get_parameter(secret_id, WithDecryption = TRUE)
-  api_parameter$Parameter$Value
+aws_get_param = function(parameter, region = 'eu-west-2', profile = ''){
+  if(profile != '') profile = paste(' --profile', profile)
+  cli_call = glue::glue('aws{profile} ssm get-parameter --name {parameter} --region {region}')
+  j = system(cli_call, intern = TRUE)
+  if(!jsonlite::validate(j)){
+    warning('failed with cli call:\n  ', cli_call)
+    return()
+  }
+  jsonlite::fromJSON(j)$Parameter$Value
 }
